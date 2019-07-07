bbef80b5507e91da0dea50d3469230306f07b1c0
Tsao-Yuan Chang <joeychang0204@gmail.com>
2019-07-06 19:47:42 -0500

    make the counter run faster

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -461,7 +461,7 @@ $('.counter-count').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     },  {
-            duration: 3000,
+            duration: 1000,
             easing: 'swing',
             step: function (now) {
                 $(this).text(Math.ceil(now));
